9aaf93c54fd51c83b4d95a4f0f4372c24936acf0
Piotr T <ptyburczy@gmail.com>
2021-03-30 11:18:24 +0200

    lab5 done

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,4 @@
-// const image = document.querySelectorAll('.image');
 const image = document.querySelectorAll('.image');
-
-const centerX = window.innerWidth / 2;
-const centerY = window.innerHeight / 2;
 
 function vmin() {
   if (window.innerHeight > window.innerWidth) {
@@ -11,26 +7,20 @@ function vmin() {
   return window.innerHeight;
 }
 
-console.log(vmin());
-
-// console.log(`${centerX}x${centerY}`);
-
 image.forEach((image) => {
   image.onclick = () => {
-    // console.log(image.getBoundingClientRect());
     image.classList.toggle('clicked');
     image.classList.toggle('mouse');
-    let rect = image.getBoundingClientRect();
+    const centerX = window.innerWidth / 2;
+    const centerY = window.innerHeight / 2;
+    const rect = image.getBoundingClientRect();
 
     if (image.classList.contains('clicked')) {
-      // image.style.transform = `scale(3) translate(${100}px, ${100}px)`;
       image.style.transform = `translate(${
         centerX - rect.x - rect.width / 2
       }px, ${centerY - rect.y - rect.height / 2}px) scale(${vmin() * 0.0045})`;
 
       image.style.transition = 'transform 0.6s';
-    } else image.style.webkitTransform = null;
-
-    // console.log(rect.x);
+    } else image.style.transform = null;
   };
 });
